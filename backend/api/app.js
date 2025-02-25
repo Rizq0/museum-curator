c07543b3a75043b19717309eecbfa42350959b9b
Joe <joe_ethan2@hotmail.com>
2025-02-25 01:02:06 +0000

    add: basic error handler

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { testRoute } = require("./controllers/test-controllers");
+const { testRoute, testError } = require("./controllers/test-controllers");
+const { errorHandler } = require("./error-handlers/error-handlers");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/test", testRoute);
+
+app.get("/api/test-error", testError);
+
+app.use(errorHandler);
 
 module.exports = { app };
--- backend/api/controllers/test-controllers.js
+++ backend/api/controllers/test-controllers.js
@@ -2,4 +2,12 @@ const testRoute = (req, res) => {
   res.status(200).json({ message: "Test route works!" });
 };
 
-module.exports = { testRoute };
+const testError = (req, res) => {
+  try {
+    throw new Error("Test error route works!", 400);
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { testRoute, testError };

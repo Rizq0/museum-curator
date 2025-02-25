c463e1aed9ec3180ae681daad84223fbb9b5c9c4
Joe <joe_ethan2@hotmail.com>
2025-02-25 00:01:42 +0000

    fix: separation of concerns

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { testRoute } = require("../controllers/test-controllers");
+const { testRoute } = require("./controllers/test-controllers");
 
 app.use(cors());
 app.use(express.json());

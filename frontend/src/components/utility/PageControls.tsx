ecda399cde5ab0f5f577037ed2a70dcf4dae0344
Joe <joe_ethan2@hotmail.com>
2025-02-26 17:35:22 +0000

    add: select gallery controls added

--- frontend/src/components/utility/PageControls.tsx
+++ frontend/src/components/utility/PageControls.tsx
@@ -1,6 +1,13 @@
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export const PageControls = ({
   previous,
@@ -16,29 +23,19 @@ export const PageControls = ({
   return (
     <div className="flex flex-row justify-between w-full">
       <div>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button className="hover:bg-dbuttonbg-pink hover:text-dbuttontext-dark bg-dbg-purple dark:bg-dbuttonbg-pink dark:hover:bg-lbg-purple dark:text-dbuttontext-dark cursor-pointer">
-              Change Art Gallery
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="bg-dbg-purple dark:bg-dbuttonbg-pink">
-            <div className="flex flex-col items-center space-y-2">
-              <Label
-                className="text-base text-lbuttonbg-white dark:text-dbuttontext-dark hover:bg-dbuttonbg-pink hover:text-dbuttontext-dark dark:hover:bg-dbg-purple dark:hover:text-dheadline-white cursor-pointer"
-                onClick={() => setCurrentGallery("harvard")}
-              >
-                Harvard Art Museum
-              </Label>
-              <Label
-                className="text-base text-lbuttonbg-white dark:text-dbuttontext-dark hover:bg-dbuttonbg-pink hover:text-dbuttontext-dark dark:hover:bg-dbg-purple dark:hover:text-dheadline-white cursor-pointer"
-                onClick={() => setCurrentGallery("cleveland")}
-              >
-                Cleveland Museum of Art
-              </Label>
-            </div>
-          </PopoverContent>
-        </Popover>
+        <Select onValueChange={(value) => setCurrentGallery(value)}>
+          <SelectTrigger className="w-[230px] cursor-pointer text-base font-semibold bg-dbg-purple text-dheadline-white hover:bg-dbuttonbg-pink hover:text-dbuttontext-dark dark:bg-dbuttonbg-pink dark:hover:bg-lbg-purple dark:text-dbuttontext-dark">
+            <SelectValue placeholder="Change Art Gallery" />
+          </SelectTrigger>
+          <SelectContent className="bg-dbg-purple text-dheadline-white dark:bg-dbuttonbg-pink dark:text-dbuttontext-dark">
+            <SelectItem value="harvard" className="text-base font-semibold">
+              Harvard Art Museum
+            </SelectItem>
+            <SelectItem value="cleveland" className="text-base font-semibold">
+              Cleveland Museum of Art
+            </SelectItem>
+          </SelectContent>
+        </Select>
       </div>
       <div className="space-x-2">
         <Button

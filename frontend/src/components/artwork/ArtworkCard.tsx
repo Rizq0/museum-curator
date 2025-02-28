78afa554b02ff6e42ac560026d057347078e762e
Joe <joe_ethan2@hotmail.com>
2025-02-28 21:22:24 +0000

    add: id added to artwork card

--- frontend/src/components/artwork/ArtworkCard.tsx
+++ frontend/src/components/artwork/ArtworkCard.tsx
@@ -3,7 +3,7 @@ import NoImage from "../../assets/No-Image-Placeholder.svg";
 interface Artwork {
   primaryimageurl: string;
   title: string;
-  artist: string;
+  id: string;
   images?: {
     print: {
       url: string;
@@ -31,11 +31,12 @@ export const ArtworkCard = ({
 
   return (
     <div className="p-4 border-2 max-w-[365px] cursor-pointer">
-      <li className="flex flex-col items-center">
+      <li>
         <div className="flex flex-col items-center">
           <h1 className="text-lg font-bold mt-2 text-center line-clamp-2">
             {artwork.title}
           </h1>
+          <h2>ID: {artwork.id}</h2>
           <img
             className="w-64 h-64 object-cover rounded-lg mt-2"
             src={getImageUrl()}

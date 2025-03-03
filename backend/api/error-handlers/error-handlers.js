3c768c74c30c6d4eb82541d78c475c872e5a24d7
Joe <joe_ethan2@hotmail.com>
2025-03-03 17:44:57 +0000

    fix: enhance error handling for validation and database errors

--- backend/api/error-handlers/error-handlers.js
+++ backend/api/error-handlers/error-handlers.js
@@ -1,8 +1,29 @@
+const {
+  ValidationError,
+  DatabaseError,
+  ForeignKeyConstraintError,
+  UniqueConstraintError,
+} = require("sequelize");
+
 const errorHandler = (err, req, res, next) => {
   console.log("Error Handler: ", err);
 
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  if (err instanceof ValidationError) {
+    statusCode = 400;
+    message = err.errors.map((error) => error.message).join(", ");
+  } else if (err instanceof ForeignKeyConstraintError) {
+    statusCode = 400;
+    message = "Invalid foreign key constraint";
+  } else if (err instanceof UniqueConstraintError) {
+    statusCode = 400;
+    message = "Unique constraint error";
+  } else if (err instanceof DatabaseError) {
+    statusCode = 500;
+    message = "Database error";
+  }
 
   res.status(statusCode).json({
     status: "error",
